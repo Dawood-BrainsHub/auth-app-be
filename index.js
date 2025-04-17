b6d759f06c3d3e87aa428ff2ea461eeb70e37e87
dawood <dawood.brainshub@gmail.com>
2025-04-17 16:23:31 +0500

    cors change

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+  
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
